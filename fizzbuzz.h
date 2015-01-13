5cc053c0fb9fc18a4ae44d7f1f260382a92153da
Ian Marshall <marshalli1@wit.edu>
2015-01-13 13:47:00 -0500

    Update

---
+++ fizzbuzz.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count = 100;
+	
+	for (int i = 0; i < count; i++)
+	{
+		if (i % 3 == 0)
+			cout << "fizz" << endl;
+		if (i % 5 == 0)
+			cout << "buzz" << endl;
+		if (i % 3 == 0 && i % 5 == 0)
+			cout << "Fizzbuzz" << endl;
+	}
+	return 0;
+	
+	
+}
